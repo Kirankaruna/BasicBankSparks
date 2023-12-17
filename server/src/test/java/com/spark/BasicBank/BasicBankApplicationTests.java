19c516645800daafb1cf8e30263fcb801fe4b6d5
Kirankaruna <kirankaruna17@gmail.com>
2023-12-17 21:44:38 +0530

    added server

---
+++ server/src/test/java/com/spark/BasicBank/BasicBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.BasicBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
